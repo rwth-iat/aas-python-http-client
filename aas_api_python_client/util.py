e42c841351ff2b7c11e21f7cbea36ae1262428b3
zrgt <garmaev@gmx.de>
2024-07-12 18:29:43 +0200

    Refactor string_to_base64url()

--- aas_api_python_client/util.py
+++ aas_api_python_client/util.py
@@ -1,19 +1,8 @@
 import base64
 
+BASE64URL_ENCODING = "utf-8"
+
 
 def string_to_base64url(input_string):
-    # Ensure the input is in string format and encode to bytes
-    if not isinstance(input_string, str):
-        raise ValueError("Input must be a string.")
-    bytes_string = input_string.encode('utf-8')
-
-    # Encode these bytes to Base64
-    base64_bytes = base64.b64encode(bytes_string)
-
-    # Convert the Base64 bytes back to string
-    base64_string = base64_bytes.decode('utf-8')
-
-    # Replace characters and remove padding for Base64URL
-    base64url_string = base64_string.replace('+', '-').replace('/', '_').rstrip('=')
-
-    return base64url_string
+    encoded = base64.urlsafe_b64encode(input_string.encode(BASE64URL_ENCODING)).decode("ascii")
+    return encoded
